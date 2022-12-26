acf4cfae539740d4f69365eb3d84c7ee8b8912aa
Shamanskiy <shamanskiy.alexander@gmail.com>
2022-12-26 19:50:40 +0100

    start building social media react app

---
+++ react/socialMediaApp/README.md
@@ -0,0 +1,6 @@
+To run dev server
+```
+npm run dev
+```
+
+HTML templates can be found [here](https://github.com/LearnWebCode/react-course)
---
+++ react/socialMediaApp/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./app/Main.js",
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "app"),
+    filename: "bundled.js"
+  },
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "app")
+    },
+    hot: true,
+    liveReload: false,
+    // always send index.html for any request URL
+    historyApiFallback: { index: "index.html" }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
+          }
+        }
+      }
+    ]
+  }
+}

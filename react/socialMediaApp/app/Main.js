16db1399095acc19684873f3563418c4feadd8d8
Shamanskiy <shamanskiy.alexander@gmail.com>
2023-01-06 22:31:36 +0100

    create post

--- react/socialMediaApp/app/Main.js
+++ react/socialMediaApp/app/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
 
 import Header from "./components/Header"
 import Home from "./components/Home"
@@ -8,6 +10,7 @@ import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexAppToken")))
@@ -17,6 +20,7 @@ function Main() {
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
         <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/create-post" element={<CreatePost />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>

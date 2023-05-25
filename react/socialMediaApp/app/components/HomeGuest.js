43e7b206c67121af2c20df74c9cc81d186779071
shamanskiy <shamanskiy.alexander@gmail.com>
2023-05-25 22:57:15 +0200

    implement validation for email and password

--- react/socialMediaApp/app/components/HomeGuest.js
+++ react/socialMediaApp/app/components/HomeGuest.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Axios from "axios"
 import { useImmerReducer } from "use-immer"
 import { CSSTransition } from "react-transition-group"
@@ -40,26 +40,62 @@ function HomeGuest() {
         }
         if (draft.username.value && !/^([a-zA-Z0-9]+)$/.test(draft.username.value)) {
           draft.username.hasErrors = true
-          draft.username.message = "Username can only contain letters and numbersj."
+          draft.username.message = "Username can only contain letters and numbers."
         }
         return
       case "usernameAfterDelay":
+        if (draft.username.value.length < 3) {
+          draft.username.hasErrors = true
+          draft.username.message = "Username must have at least 3 characters."
+        }
+        if (!draft.username.hasErrors) {
+          draft.username.checkCount++
+        }
         return
       case "usernameUniqueResults":
+        if (action.value) {
+          draft.username.hasErrors = true
+          draft.username.isUnique = false
+          draft.username.message = "That username is already taken."
+        } else {
+          draft.username.isUnique = true
+        }
         return
       case "emailImmediately":
         draft.email.hasErrors = false
         draft.email.value = action.value
         return
       case "emailAfterDelay":
+        if (!/^\S+@\S+$/.test(draft.email.value)) {
+          draft.email.hasErrors = true
+          draft.email.message = "You must provide a valid email address."
+        }
+        if (!draft.email.hasErrors) {
+          draft.email.checkCount++
+        }
         return
       case "emailUniqueResults":
+        if (action.value) {
+          draft.email.hasErrors = true
+          draft.email.isUnique = false
+          draft.email.message = "That email is already taken."
+        } else {
+          draft.email.isUnique = true
+        }
         return
       case "passwordImmediately":
         draft.password.hasErrors = false
         draft.password.value = action.value
+        if (draft.password.value.length > 50) {
+          draft.password.hasErrors = true
+          draft.password.message = "Password cannot exceed 50 characters."
+        }
         return
       case "passwordAfterDelay":
+        if (draft.password.value.length < 12) {
+          draft.password.hasErrors = true
+          draft.password.message = "Password must have at least 12 characters."
+        }
         return
       case "submitForm":
         return
@@ -67,6 +103,67 @@ function HomeGuest() {
   }
 
   const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
+  useEffect(() => {
+    if (state.username.value) {
+      const delay = setTimeout(() => dispatch({ type: "usernameAfterDelay" }), 800)
+      return () => clearTimeout(delay)
+    }
+  }, [state.username.value])
+
+  useEffect(() => {
+    if (state.email.value) {
+      const delay = setTimeout(() => dispatch({ type: "emailAfterDelay" }), 800)
+      return () => clearTimeout(delay)
+    }
+  }, [state.email.value])
+
+  useEffect(() => {
+    if (state.password.value) {
+      const delay = setTimeout(() => dispatch({ type: "passwordAfterDelay" }), 800)
+      return () => clearTimeout(delay)
+    }
+  }, [state.password.value])
+
+  useEffect(() => {
+    if (state.username.checkCount) {
+      const ourRequest = Axios.CancelToken.source()
+      async function checkUsername() {
+        try {
+          const response = await Axios.post(
+            "/doesUsernameExist",
+            { username: state.username.value },
+            { cancelToken: ourRequest.token }
+          )
+          dispatch({ type: "usernameUniqueResults", value: response.data })
+        } catch (e) {
+          console.log("there was a problem or the request was canceled")
+        }
+      }
+      checkUsername()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.username.checkCount])
+
+  useEffect(() => {
+    if (state.email.checkCount) {
+      const ourRequest = Axios.CancelToken.source()
+      async function checkEmail() {
+        try {
+          const response = await Axios.post("/doesEmailExist", { email: state.email.value }, { cancelToken: ourRequest.token })
+          dispatch({ type: "emailUniqueResults", value: response.data })
+        } catch (e) {
+          console.log("there was a problem or the request was canceled")
+        }
+      }
+      checkEmail()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.email.checkCount])
 
   function handlerSubmit(e) {
     e.preventDefault()
@@ -114,6 +211,9 @@ function HomeGuest() {
                 placeholder="you@example.com"
                 autoComplete="off"
               />
+              <CSSTransition in={state.email.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.email.message}</div>
+              </CSSTransition>
             </div>
             <div className="form-group">
               <label htmlFor="password-register" className="text-muted mb-1">
@@ -127,6 +227,9 @@ function HomeGuest() {
                 type="password"
                 placeholder="Create a password"
               />
+              <CSSTransition in={state.password.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.password.message}</div>
+              </CSSTransition>
             </div>
             <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
               Sign up for ComplexApp
